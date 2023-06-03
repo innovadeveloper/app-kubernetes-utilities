8944ca3988b39df6d0180d19c78ead90a44c3d30
Kenny Baltazar alanoca <developer.innovaappstar@gmail.com>
2023-06-03 12:33:04 -0500

    second commit

--- groovy-utility/src/main/groovy/com/innovaappstar/groovy/utility/utils/FileUtils.groovy
+++ groovy-utility/src/main/groovy/com/innovaappstar/groovy/utility/utils/FileUtils.groovy
@@ -2,14 +2,29 @@ package com.innovaappstar.groovy.utility.utils
 
 import javax.swing.JFileChooser
 import java.awt.Component
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class FileUtils {
     static String getMessage(String message){
         def pwdCommand = "pwd".execute().text
-        def lsCommand = "ls -l".execute().text
+        def lsCommand = "ls".execute().text.split("\n")
         println(pwdCommand)
         println("___________")
         println(lsCommand)
+
+        def a= Paths.get(pwdCommand)toAbsolutePath()
+        def currentDir = Paths.get(pwdCommand).toAbsolutePath().toString()
+        def files2 = Files.list(Paths.get(currentDir))
+
+        def files = Files.list(Paths.get(currentDir))
+                .filter { Files.isRegularFile(it) }
+                .map { it.getFileName().toString() }
+                .toList()
+
+        files.each { fileName ->
+            println(fileName)
+        }
         return "hola " + message
     }
 
